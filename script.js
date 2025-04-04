29028c1965664eefcb9a7419dc4d25eaaaf356da
Maruka <emanuelle_carollina@hotmail.com>
2025-04-04 01:55:08 -0300

    Loop rodadas

---
+++ script.js
@@ -0,0 +1,13 @@
+function jogar() {
+
+    rodada = 1
+    while(rodada <= 3) {
+        console.log("Rodada: " + rodada)
+        rodada = rodada + 1
+
+        escolhaJogador = prompt("Nível "+rodada+", vidro 1, 2 ou 3?")
+    }
+
+}
+
+
