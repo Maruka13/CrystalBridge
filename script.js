c1663bca19b038a8f4578559f5ab69e8399b551d
Maruka <emanuelle_carollina@hotmail.com>
2025-04-06 23:10:02 -0300

    correção num níveis

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ function jogar() {
     rodada = 1
     while(rodada <= 3) {
         console.log("Rodada: " + rodada)
-        rodada = rodada + 1
 
         escolhaJogador = prompt("Nível "+rodada+", vidro 1, 2 ou 3?")
         pisoQuebrado = Math.floor(Math.random()*3) +1;
+
+        rodada = rodada + 1
     }
 
 }
